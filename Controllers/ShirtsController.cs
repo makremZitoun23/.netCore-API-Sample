1b7cfaec14ac804bc4d5a38736a98401eb897cbc
makremZitoun23 <makrem.zitoun@outlook.fr>
2024-06-28 00:00:09 +0100

    tesr

--- Controllers/ShirtsController.cs
+++ Controllers/ShirtsController.cs
@@ -27,7 +27,7 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public string CreateShort(int id)
         {
-            return $"Update shortss ID : {id}";
+            return $"Update shortsss ID : {id}";
 
         }
 
